8aeac3b2833c96701bc8d48c37308fbdf57cffb3
Andrew <andrewunifei@gmail.com>
2024-10-27 19:33:45 -0300

    [ADD] Players page

--- app/client/src/app/players/explore/page.js
+++ app/client/src/app/players/explore/page.js
@@ -11,8 +11,6 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Divider from '@mui/material/Divider';
-import { getTeamsGamesInfo, getTeamsGoalsInfo } from '@/app/api/team';
-import { getTeamsCardsInfo } from '@/app/api/team';
 import { getPlayerInfo } from '@/app/api/player';
 
 function getTitle(logo, name) {
@@ -60,48 +58,73 @@ function ExplorePlayer() {
         <Paper sx={{borderRadius: 0, p: 2, width: '100%', height: '100vh', overflow: 'auto' }} elevation={1}> 
             <PageContainer maxWidth="xl" breadcrumbs={getBread()} sx={{marginBottom: '150px'}} >
                 {getTitle(photo, name)}
-                <Grid container spacing={3} >
-                    <Grid size={6} sx={{border: '2px', borderColor: '#fff'}}>
-                        <Paper sx={{borderRadius: 3, p: 5}} elevation={3}>
-                            <p style={{fontSize: '24px', font: 'roboto', fontWeight: '100'}}>Identificação Geral</p>
-                            <Divider orientation="horizontal" style={{marginBottom: '40px'}} />
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Nome completo: </span>
-                                {(playerInfo['first_name'] && playerInfo['last_name']) ? (playerInfo['first_name'] + ' ' + playerInfo['last_name']) : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Nacionalidade: </span>
-                                {(playerInfo['nationality']) ? (playerInfo['nationality']) : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Idade: </span>
-                                {(playerInfo['age'])? (playerInfo['age']) : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Data de nascimento: </span>
-                                {(playerInfo['birth'])? (playerInfo['birth']['date'] ? playerInfo['birth']['date'] : 'N/A') : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Local de nascimento: </span>
-                                {(playerInfo['birth'])? (playerInfo['birth']['place'] ? playerInfo['birth']['place'] : 'N/A') : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Altura: </span>
-                                {(playerInfo['height'])? (playerInfo['height']) : ''}
-                            </p>
-                            <p>
-                                <span style={{fontWeight: 'bold'}}>Peso: </span>
-                                {(playerInfo['weight'])? (playerInfo['weight']) : ''}
-                            </p>
-                        </Paper>
+                {playerInfo['player_id'] ?
+                (
+                    <Grid container spacing={3} >
+                        <Grid size={6} sx={{border: '2px', borderColor: '#fff'}}>
+                            <Paper sx={{borderRadius: 3, p: 5}} elevation={3}>
+                                <p style={{fontSize: '24px', font: 'roboto', fontWeight: '100'}}>Identificação Geral</p>
+                                <Divider orientation="horizontal" style={{marginBottom: '40px'}} />
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Nome completo: </span>
+                                    {(playerInfo['first_name'] && playerInfo['last_name']) ? (playerInfo['first_name'] + ' ' + playerInfo['last_name']) : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Nacionalidade: </span>
+                                    {(playerInfo['nationality']) ? playerInfo['nationality'] : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Idade: </span>
+                                    {(playerInfo['age']) ? playerInfo['age'] : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Data de nascimento: </span>
+                                    {(playerInfo['birth']) ? (playerInfo['birth']['date'] ? playerInfo['birth']['date'] : 'N/A') : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Local de nascimento: </span>
+                                    {(playerInfo['birth']) ? (playerInfo['birth']['place'] ? playerInfo['birth']['place'] : 'N/A') : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Altura: </span>
+                                    {(playerInfo['height']) ? playerInfo['height'] : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Peso: </span>
+                                    {(playerInfo['weight']) ? playerInfo['weight'] : 'N/A'}
+                                </p>
+                                <p>
+                                    <span style={{fontWeight: 'bold'}}>Posição: </span>
+                                    {(playerInfo['position']) ? playerInfo['position'] : 'N/A'}
+                                </p>
+                            </Paper>
+                        </Grid>
+                        <Grid size={6} sx={{border: '2px', borderColor: '#fff'}}>
+                            <Paper sx={{borderRadius: 3, p: 5}} elevation={3}>
+                                <p style={{fontSize: '24px', font: 'roboto', fontWeight: '100'}}>Estatísticas na Liga</p>
+                                <Divider orientation="horizontal" style={{marginBottom: '40px'}} />
+                                    {
+                                        playerInfo['games_appearences'] ?
+                                            (
+                                                <>
+                                                    <p><span style={{fontWeight: 'bold'}}>Jogos jogados: </span>{playerInfo['games_appearences']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Minutos jogados: </span>{playerInfo['minutes_played_total'] == null ? 'N/A' : playerInfo['minutes_played_total']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Gols: </span>{playerInfo['goals_total'] == null ? 'N/A' : playerInfo['goals_total']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Passes: </span>{playerInfo['passes_total'] == null ? 'N/A' : playerInfo['passes_total']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Passes chaves: </span>{playerInfo['passes_key'] == null ? 'N/A' : playerInfo['passes_key']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Cartões amarelos: </span>{playerInfo['cards_yellow_total'] == null ? 'N/A' : playerInfo['cards_yellow_total']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Cartões vermelhos: </span>{playerInfo['cards_red_total'] == null ? 'N/A' : playerInfo['cards_red_total']}</p>
+                                                    <p><span style={{fontWeight: 'bold'}}>Nota: </span>{playerInfo['rating'].slice(0,4)}</p>
+                                                </>
+                                            )
+                                            : (playerInfo['games_appearences'] == 0 ) ?
+                                                (<p><span style={{fontWeight: 'bold'}}>Jogos jogados: </span>0</p>) 
+                                                : (<p><span style={{fontWeight: 'bold'}}>Jogos jogados: </span>N/A</p>)
+                                    }
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid size={6} sx={{border: '2px', borderColor: '#fff'}}>
-                        <Paper sx={{borderRadius: 3, p: 5}} elevation={3}>
-                            <Box sx={{height: 300, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                            </Box>
-                        </Paper>
-                    </Grid>
-                </Grid>
+                ) : '' }
             </PageContainer>
         </Paper>
     )
